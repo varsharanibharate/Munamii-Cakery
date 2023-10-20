4c5241c6a1a5ae7a50b41c0636cbe1a07b59f2a7
Varsharani Bharate <varsharanibharate@gmail.com>
2023-10-20 13:29:32 +0200

    Base Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+function mouseOverCupcake1() {
+    document.getElementById("cupcake1").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1506070366.jpg";
+}
+
+function mouseOutCupcake1() {
+    document.getElementById("cupcake1").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1498969810.jpg";
+}
+
+function mouseOverCupcake2() {
+    document.getElementById("cupcake2").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/3587653815.jpg";
+}
+
+function mouseOutCupcake2() {
+    document.getElementById("cupcake2").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/3587618652.jpg";
+}
+
+function mouseOverCupcake3() {
+    document.getElementById("cupcake3").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/3587653815.jpg";
+}
+
+function mouseOutCupcake3() {
+    document.getElementById("cupcake3").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/3587614724.jpg";
+}
+
+function mouseOverCupcake4() {
+    document.getElementById("cupcake4").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1506070366.jpg";
+}
+
+function mouseOutCupcake4() {
+    document.getElementById("cupcake4").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1498969807.jpg";
+}
+
+function mouseOverCupcake5() {
+    document.getElementById("cupcake5").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1506070366.jpg";
+}
+
+function mouseOutCupcake5() {
+    document.getElementById("cupcake5").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1498969801.jpg";
+}
+
+function mouseOverCupcake6() {
+    document.getElementById("cupcake6").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/3587653815.jpg";
+}
+
+function mouseOutCupcake6() {
+    document.getElementById("cupcake6").src = "https://live.staticflickr.com/8269/8756645330_7978106b20_w.jpg";
+}
+
+function mouseOverCupcake7() {
+    document.getElementById("cupcake7").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1506079054.jpg";
+}
+
+function mouseOutCupcake7() {
+    document.getElementById("cupcake7").src = "https://live.staticflickr.com/5330/8755524555_9f7891137e.jpg";
+}
+
+function mouseOverCupcake8() {
+    document.getElementById("cupcake8").src = "https://d2j6dbq0eux0bg.cloudfront.net/images/16830190/1506070341.jpg";
+}
+
+function mouseOutCupcake8() {
+    document.getElementById("cupcake8").src = "https://live.staticflickr.com/5462/8755524049_afd76b6593_b.jpg";
+}
+
